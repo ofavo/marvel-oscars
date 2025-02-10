00d6fd28e9864383452bc13f1f3e3234a58b5d9f
Oscar Villamizar <33492490+ofavo@users.noreply.github.com>
2025-02-10 19:18:51 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'marvel-oscars',
+  webDir: 'www'
+};
+
+export default config;
